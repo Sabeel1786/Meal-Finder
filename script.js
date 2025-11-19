55ae3138162d57d101aedf7690212a163f4abfa0
Sabeel1786 <sabeeltanveer@gmail.com>
2025-11-20 02:50:28 +0530

    worked on home page layout

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+
 async function fetchData() {
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     const data = await response.json()
-    console.log(data);
+    console.log(data.categories);
+    DataDisplay(data.categories)
 }
 fetchData()
+
+
+function DataDisplay(products) {
+    const Category = document.querySelector(".Category") 
+    let product = "";
+    products.forEach((prod) => {
+        product += `
+        <div class="CategoryProducts">
+        <h5 id="title">${prod.strCategory}</h5>
+        <img src="${prod.strCategoryThumb}" alt="${prod.strCategory}">
+        <a href="${prod.idCategory}"></a>
+        </div>`;
+    })
+     Category.innerHTML = product
+}
+
+
+
